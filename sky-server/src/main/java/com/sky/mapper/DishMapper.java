c990817677630cead511670847249edabc599b7d
pamrock <1245945665@qq.com>
2025-02-07 11:18:44 +0800

    删除菜品代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,16 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -17,4 +23,26 @@ public interface DishMapper {
     * */
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+
+    /*
+    * 菜品分页查询
+    * */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /*
+    * 根据主键查询菜品
+    * */
+    @Select("select * from dish where id = #{id}")
+    Dish getById(Long id);
+
+    /*
+    * 根据主键删除菜品数据
+    * */
+    @Delete("delete from dish where id = #{id}")
+    void DeleteById(Long id);
+
+    /*
+    * 根据菜品id集合批量删除菜品数据
+    * */
+    void DeleteByIds(List<Long> ids);
 }
