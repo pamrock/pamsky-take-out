0badf027e448aee873a37e14c5166c258eefa6a6
pamrock <1245945665@qq.com>
2025-02-19 11:22:23 +0800

    缓存菜品代码开发

--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,8 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
     /*
     * 根据分类id查询菜品
     * */
@@ -37,7 +40,21 @@ public class DishController {
     @ApiOperation("根据分类id查询菜品")
     public Result<List<DishVO>> list(Long categoryId) {
         log.info("根据分类id查询菜品");
-        List<DishVO> list = dishService.listWithFlavor(categoryId);
+        //构造redis中的key
+        String key = "dish_" + categoryId;
+
+        //查询redis中是否存在菜品数据
+        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if(list != null && list.size() > 0) {
+            //如存在则直接返回
+            return Result.success(list);
+
+        }
+
+        //如不存在则查询数据库并放入redis中
+        list = dishService.listWithFlavor(categoryId);
+        redisTemplate.opsForValue().set(key, list);
+        
         return Result.success(list);
     }
 }
