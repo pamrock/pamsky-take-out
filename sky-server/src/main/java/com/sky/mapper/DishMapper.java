6a6c7b952a60e32f4e0b40a09f24a6eed1e453fb
pamrock <1245945665@qq.com>
2025-03-25 12:41:23 +0800

    工作台相关代码

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -56,4 +57,11 @@ public interface DishMapper {
     * 根据分类id查询菜品
     * */
     List<Dish> list(Dish dish);
+
+        /**
+         * 根据条件统计菜品数量
+         * @param map
+         * @return
+         */
+    Integer countByMap(Map map);
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -46,4 +47,11 @@ public interface SetmealMapper {
     * 根据分类id查询套餐
     * */
     List<Setmeal> getByCategoryId(Setmeal setmeal);
+
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
